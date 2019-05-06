7ffccad28b41fa25cd042b76ba820f3f3881cfd7
Chongnaitor <idv18c.jchong@uartesdigitales.edu.mx>
2019-05-05 19:55:05 -0500

    Nuevos programas :D

---
+++ Parcial 1/Practicas/Numero Par/NumeroPar.cpp
@@ -0,0 +1,30 @@
+//Este programa sirve para saber si un numero es par
+#include <iostream>
+#include <bitset>
+using namespace std;
+int main()
+{
+	cout << "Escribe el numero que quieres comprobar, si pones letras se tomaran como cero" << endl;
+	int NumeroAComprobar;//Variable de tipo entero que se usara para comprobar si el numero del usuario es par
+	cin >> NumeroAComprobar;//Le pedimos al usuario que ingrese un numero
+	string NumeroBinario = bitset<30>(NumeroAComprobar).to_string();//Pasamos a binario el numero en una variable de tipo string, el numero de los corchetes angulares es el numero de digitos binarios que pueden tener el numero del usuario
+	if (NumeroBinario == "000000000000000000000000000000")//El identificador de cero equivale a trienta ceros por lo que comente anteriormente, por eso lo estableci
+	{
+		cout << "el 0 no es par ni es inpar" << endl;
+	}
+	else
+	{
+		if (NumeroBinario.back() == '0')//Evalua si hay un cero al final significa que es par
+		{
+			cout << NumeroAComprobar << " si es par" << endl;
+		}
+		else if (NumeroBinario.back() == '1')//Si hay uno no es par
+		{
+			cout << NumeroAComprobar << " no es par" << endl;
+		}
+	
+	}
+	
+	cin.ignore();//Evita que se cierre el programa automaticamente
+	cin.get();
+}
