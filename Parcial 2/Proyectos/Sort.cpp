1bc16085e912b0f6d2faa57407afb0fafcef6e17
Chongnaitor <idv18c.jchong@uartesdigitales.edu.mx>
2019-07-15 18:09:46 -0500

    Update Sort.cpp
    Lo que olvice

--- Parcial 2/Proyectos/Sort.cpp
+++ Parcial 2/Proyectos/Sort.cpp
@@ -15,7 +15,76 @@ DoubleNode * Sort::swap(DoubleNode *N1, DoubleNode *N2)
 	return nullptr;
 }
 
-void Sort::bubble(SingleList &)
+#include "Sort.h"
+#include "List.h"
+#include "Node.h"
+
+SingleNode * Sort::swap(SingleNode *N1, SingleNode *N2)
+{
+	SingleNode*temp = N1;
+	N1 = N2;
+	N2 = temp;
+
+}
+
+DoubleNode * Sort::swap(DoubleNode *N1, DoubleNode *N2)
+{
+	return nullptr;
+}
+
+SingleNode * Sort::swap(SingleNode * a, SingleNode * b) {
+	a->next = b->next;
+	b->next = a;
+	return b;
+}
+
+/* Function to sort a single linked list using bubble sort */
+void Sort::bubble(SingleList&start) {
+
+	SingleNode ** iter;
+	SingleNode * a, *b;
+	unsigned int size = start.size();
+	bool swapped = false;
+
+	for (unsigned int i = 0; i < size; i++) {
+		iter = start.start;
+		for (unsigned int j = 0; j < size - i - 1; j++) {
+			a = *iter;
+			b = a->next;
+
+			if (*a > *b) {
+				swapped = true;
+				*iter = swap(a, b);
+			}
+
+			iter = &(*iter)->next;
+		}
+		if (!swapped)
+			break;
+	}
+}
+void Sort::bubble(DoubleList &)
+{
+}
+
+void Sort::quick(SingleList &Lmao)
+{
+
+	unsigned int pivote = Lmao.s;
+
+
+
+
+
+}
+
+
+
+
+
+
+
+void Sort::quick(DoubleList &)
 {
 }
 
