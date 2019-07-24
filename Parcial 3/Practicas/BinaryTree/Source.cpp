623d7de503d26512565ffd31b0de2690fa58fbfa
Chongnaitor <idv18c.jchong@uartesdigitales.edu.mx>
2019-07-23 21:08:42 -0500

    Update Source.cpp

--- Parcial 3/Practicas/BinaryTree/Source.cpp
+++ Parcial 3/Practicas/BinaryTree/Source.cpp
@@ -1,12 +1,11 @@
-
 #include <iostream>
-
+#include<string>
 using namespace std;
-
 
 struct Nodo {
 	int Dato;
-
+	string Nombre;
+	string Apellido;
 	Nodo *izquierda;
 	Nodo *derecha;
 };
@@ -15,23 +14,19 @@ class BinaryTree {
 public:
 	BinaryTree();
 	~BinaryTree();
-
-	void insert(int Dato);
-	Nodo *buscar(int Dato);
+	void postordenado_print(Nodo *rama);
+	void preordenado_print(Nodo *rama);
+	void insert(int Dato, string Nombre, string Apellido);
 	void destruir_tree();
 	void enorden_print();
 	void postordenado_print();
 	void preordenado_print();
 	void destruir_tree(Nodo *rama);
-	void insert(int Dato, Nodo *rama);
-	Nodo *buscar(int Dato, Nodo *rama);
+	void insert(int Dato, string Nombre, string Apellido, Nodo *rama);
 	void enorden_print(Nodo *rama);
-	void postordenado_print(Nodo *rama);
-	void preordenado_print(Nodo *rama);
 
 	Nodo *raiz;
 };
-
 
 BinaryTree::BinaryTree() {
 	raiz = nullptr;
@@ -41,7 +36,7 @@ BinaryTree::~BinaryTree() {
 	destruir_tree();
 }
 
-void BinaryTree::destruir_tree(Nodo *rama) {
+void BinaryTree::destruir_tree(Nodo*rama) {
 	if (rama != nullptr) {
 		destruir_tree(rama->izquierda);
 		destruir_tree(rama->derecha);
@@ -49,75 +44,78 @@ void BinaryTree::destruir_tree(Nodo *rama) {
 	}
 }
 
-void BinaryTree::insert(int Dato, Nodo *rama) {
+void BinaryTree::insert(int Dato, string Nombre, string Apellido, Nodo*rama)
+{
 
-	if (Dato < rama->Dato) {
-		if (rama->izquierda != nullptr) {
-			insert(Dato, rama->izquierda);
-		}
-		else {
-			rama->izquierda = new Nodo;
-			rama->izquierda->Dato = Dato;
-			rama->izquierda->izquierda = nullptr;
-			rama->izquierda->derecha = nullptr;
-		}
+
+	if (rama->izquierda != nullptr) {
+		insert(Dato, Nombre, Apellido, rama->izquierda);
 	}
-	else if (Dato >= rama->Dato) {
-		if (rama->derecha != nullptr) {
-			insert(Dato, rama->derecha);
-		}
-		else {
-			rama->derecha = new Nodo;
-			rama->derecha->Dato = Dato;
-			rama->derecha->derecha = nullptr;
-			rama->derecha->izquierda = nullptr;
-		}
+	else {
+		rama->izquierda = new Nodo;
+		rama->izquierda->Dato = Dato;
+		rama->izquierda->Nombre = Nombre;
+		rama->izquierda->Apellido = Apellido;
+		rama->izquierda->izquierda = nullptr;
+		rama->izquierda->derecha = nullptr;
+	}
+
+
+	if (rama->derecha != nullptr) {
+		insert(Dato, Nombre, Apellido, rama->derecha);
+	}
+	else {
+		rama->derecha = new Nodo;
+		rama->derecha->Dato = Dato;
+		rama->derecha->Nombre = Nombre;
+		rama->derecha->Apellido = Apellido;
+		rama->derecha->derecha = nullptr;
+		rama->derecha->izquierda = nullptr;
 	}
 
 }
 
-void BinaryTree::insert(int Dato) {
+void BinaryTree::insert(int Dato, string Nombre, string Apellido) {
 	if (raiz != nullptr) {
-		insert(Dato, raiz);
+		insert(Dato, Nombre, Apellido, raiz);
 	}
 	else {
 		raiz = new Nodo;
 		raiz->Dato = Dato;
+		raiz->Nombre = Nombre;
+		raiz->Apellido = Apellido;
 		raiz->izquierda = nullptr;
 		raiz->derecha = nullptr;
 	}
 }
 
-
-
 void BinaryTree::destruir_tree() {
 	destruir_tree(raiz);
 }
 
-void BinaryTree::enorden_print(Nodo *rama) {
+void BinaryTree::enorden_print(Nodo*rama) {
 	if (rama != nullptr) {
 		enorden_print(rama->izquierda);
-		cout << rama->Dato << ",";
+		cout << rama->Dato << "," << rama->Nombre << "," << rama->Apellido << ",";
 		enorden_print(rama->derecha);
 	}
 }
+
 void BinaryTree::enorden_print() {
 	enorden_print(raiz);
 	cout << "\n";
 }
-
-
 
 void BinaryTree::postordenado_print() {
 	postordenado_print(raiz);
 	cout << "\n";
 }
 
-void BinaryTree::postordenado_print(Nodo *rama) {
+void BinaryTree::postordenado_print(Nodo*rama) {
 	if (rama != nullptr) {
 		enorden_print(rama->izquierda);
 		enorden_print(rama->derecha);
-		cout << rama->Dato << ",";
+		cout << rama->Dato << "," << rama->Nombre << "," << rama->Apellido << ",";
 	}
 }
 
@@ -126,7 +124,7 @@ void BinaryTree::preordenado_print() {
 	cout << "\n";
 }
 
-void BinaryTree::preordenado_print(Nodo *rama) {
+void BinaryTree::preordenado_print(Nodo*rama) {
 	if (rama != nullptr) {
 		cout << rama->Dato << ",";
 		enorden_print(rama->izquierda);
@@ -140,14 +138,20 @@ int main() {
 
 
 	int Dato;
+	string temp;
+	string temp2;
 	int Ciclo = 0;
 	cout << "Cuantos datos quieres meter" << endl;
 	cin >> Ciclo;
 	for (int i = 0; i < Ciclo; i++)
 	{
-		cout << "Inserte dato:" << endl;
+		cout << "Inserte edad:" << endl;
 		cin >> Dato;
-		tree->insert(Dato);
+		cout << "Escribe el nombre" << endl;
+		cin >> temp;
+		cout << "Escribe el apellido" << endl;
+		cin >> temp2;
+		tree->insert(Dato, temp, temp2);
 		cout << "Dato ingresado" << endl;
 
 	}
